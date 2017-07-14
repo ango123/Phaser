583259c07b815d9ec5e050a337191ab280760e22
ango123 <annie.go100@gmail.com>
2017-07-14 12:59:07 -0700

    commit

--- MainState.js
+++ MainState.js
@@ -1,6 +1,7 @@
 var mainState = {
 	preload: function() {
 		this.game.load.spritesheet("playerSheet", "Sprite.png", 1841/4, 2400/4)
+		this.game.load.spritesheet("enemySheet", "enemy.png", 64, 64)
 	},
 	create: function (){
 		this.game.world.setBounds(0,0,2500,10000);
@@ -66,30 +67,55 @@ var mainState = {
 ];
 		
 
-for (var i = 0; i < level.length; i++) {
-for (var j = 0; j < level[i].length; j++) {
+	for (var i = 0; i < level.length; i++) {
+		for (var j = 0; j < level[i].length; j++) {
 
-if (level[i][j] == 'x') {
-var wall = this.game.add.sprite(30+20*j, 30+20*i, 'wall');
-this.walls.add(wall);
-wall.body.immovable = true; 
-}
+			if (level[i][j] == 'x') {
+				var wall = this.game.add.sprite(30+20*j, 30+20*i, 'wall');
+				this.walls.add(wall);
+				wall.body.immovable = true; 
+			}
 
-else if (level[i][j] == 'o') {
-var coin = this.game.add.sprite(30+20*j, 30+20*i, 'coin');
-this.coins.add(coin);
-}
+			else if (level[i][j] == 'o') {
+				var coin = this.game.add.sprite(30+20*j, 30+20*i, 'coin');
+				this.coins.add(coin);
+			}
 
-else if (level[i][j] == '!') {
-var enemy = this.game.add.sprite(30+20*j, 30+20*i, 'lava');
-this.lavas.add(enemy);
-}
-else if (level[i][j] == 'b') {
-var monster = this.game.add.sprite(30+20*j, 30+20*i, 'enemy');
-this.enemy.add(monster);
-var tween = this.game.add.tween(this.enemy).to( { x: 200 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
-}
-}
+			else if (level[i][j] == '!') {
+				var enemy = this.game.add.sprite(30+20*j, 30+20*i, 'lava');
+				this.lavas.add(enemy);
+			}
+
+			else if (level[i][j] == 'b') {
+				var monster = this.game.add.sprite(30+20*j, 5+20*i, 'enemySheet');
+				var enemy = this.enemy.add(monster);
+				var tween = this.game.add.tween(this.enemy).to( 
+					{ x: 200 }, 
+					2000, 
+					Phaser.Easing.Linear.None, 
+					true, 
+					0, 
+					1000, 
+					true
+				);
+				window.tween = tween
+				let name = "walk_right"
+				tween.onRepeat.add(() => {
+					console.log(name)
+					if (name === "walk_left") {
+						monster.animations.play("walk_right", 25, true)
+						name = "walk_right"
+					} else {
+						monster.animations.play("walk_left", 25, true)
+						name = "walk_left"
+					}
+				})
+				monster.animations.add("walk_left", [9, 10, 11, 12, 13, 14, 15, 16, 17], 25)
+				monster.animations.add("walk_right", [27, 28, 29, 30, 31, 32, 33, 34, 35], 25)
+				monster.animations.play("walk_right", 25, true)
+				monster.scale.setTo(0.75)		
+			}
+	}
 }
 	},
 ////createenemies: function () {
@@ -119,13 +145,15 @@ var tween = this.game.add.tween(this.enemy).to( { x: 200 }, 2000, Phaser.Easing.
 		this.updateKeys();
 		if(this.cursor.left.isDown){
 		   this.player.body.velocity.x = -200;
-		   this.player. 
+		   this.player.animations.play("walk_left", 20)
 		}
 		else if(this.cursor.right.isDown){
 			this.player.body.velocity.x = 200;
+			this.player.animations.play("walk_right", 20)
 		}
 		else{
 			this.player.body.velocity.x =0;
+			this.player.animations.play("stand")
 		}
 		if(this.cursor.up.isDown && this.player.body.touching.down){
 		   this.player.body.velocity.y = -600;
@@ -136,7 +164,6 @@ updateKeys: function (){
 	this.p1_health.callAll('kill');
 	var j = 0;
 	for(var i = 0; i< this.score; i++){
-		console.log("creating group", i)
 		this.p1_health.create(j, 10, 'coin');
 		this.p1_health.fixedToCamera = true;
 		//this.p1_health.anchor.setTo(0.5,0.5);
